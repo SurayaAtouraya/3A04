efe5e01cb6ccc07e8d7aa780a57390c37b994fe6
Hamid Ghasemi <ghasemih@mcmaster.ca>
2019-04-05 12:59:31 -0300

    SpaceShip UI

---
+++ SpaceShip/SpaceShip/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace SpaceShip
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
+            this.Setting = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Setting
+            // 
+            this.Setting.BackColor = System.Drawing.Color.LightYellow;
+            this.Setting.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Setting.Font = new System.Drawing.Font("Matura MT Script Capitals", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Setting.Location = new System.Drawing.Point(447, 417);
+            this.Setting.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Setting.Name = "Setting";
+            this.Setting.Size = new System.Drawing.Size(228, 94);
+            this.Setting.TabIndex = 2;
+            this.Setting.Text = "Setting";
+            this.Setting.UseVisualStyleBackColor = false;
+            this.Setting.Click += new System.EventHandler(this.Setting_Click);
+            // 
+            // Start
+            // 
+            this.Start.AutoSize = true;
+            this.Start.BackColor = System.Drawing.Color.LightYellow;
+            this.Start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Start.Font = new System.Drawing.Font("Matura MT Script Capitals", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.Location = new System.Drawing.Point(207, 417);
+            this.Start.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(232, 94);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.LightYellow;
+            this.Exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Exit.Font = new System.Drawing.Font("Matura MT Script Capitals", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.Location = new System.Drawing.Point(683, 417);
+            this.Exit.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(228, 94);
+            this.Exit.TabIndex = 3;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkKhaki;
+            this.label1.Location = new System.Drawing.Point(4, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 123);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "S-Pace";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightYellow;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1092, 531);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Setting);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Font = new System.Drawing.Font("Matura MT Script Capitals", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "form1";
+            this.Text = "Spaceship";
+            this.Load += new System.EventHandler(this.form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Setting;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SpaceShip/SpaceShip/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SpaceShip
+{
+    public partial class form1 : Form
+    {
+        System.Media.SoundPlayer play = new System.Media.SoundPlayer();
+        public form1()
+        {
+            InitializeComponent();
+            play.SoundLocation  = "hypnotize.wav";
+            play.Play();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 f = new Form3();
+            f.ShowDialog();
+        }
+
+        private void Setting_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 f = new Form2();
+            f.ShowDialog();
+            this.Show();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
